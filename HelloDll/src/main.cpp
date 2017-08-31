883a593a957eb77f59a7d0f150288cb1bc0cff98
BuildTools <unconfigured@null.spigotmc.org>
2017-08-31 20:33:12 +0200

    it compiles now

--- HelloDll/src/main.cpp
+++ HelloDll/src/main.cpp
@@ -2,18 +2,6 @@
 #include <cstdio>
 #include <iostream>
 #include <Windows.h>
-#include <detours.h>
-
-double(__cdecl *originalFunction)(double); //Pointer to the function we are going to hook, must be declared same as original(returns double and takes double as argument)
-
-//Our modified function code that is going to be executed
-//before continuing to the code of original function
-double hookedFunction(double a)
-{
-  std::cout << "original function: argument = " << a << std::endl; //we can access arguments passed to original function
-  a = 50.1337;                                                     //Modify arguments
-  return originalFunction(a);                                      //before returning to normal execution of function
-}
 
 extern "C" BOOL APIENTRY DllMain(
     HMODULE hModule,
@@ -23,7 +11,7 @@ extern "C" BOOL APIENTRY DllMain(
   switch (ul_reason_for_call)
   {
   case DLL_PROCESS_ATTACH:
-    originalFunction = (double(__cdecl *)(double))DetourFunction((PBYTE)0x401470, (PBYTE)hookedFunction); //Magic
+    
     MessageBox(NULL, "Abcd", "Test", 0);
     break;
   }
---
+++ SlaveTestProgram/CMakeFiles/slave.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/slave.dir/src/main.cpp.obj"
+  "bin/slave.pdb"
+  "bin/slave.exe"
+  "bin/slave.exe.manifest"
+  "libslave.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/slave.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
